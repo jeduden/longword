d5c2409e4aa3b93c894a7e1d5ef0dc380b52cb46
Jan-Eric Duden <jeduden@gmail.com>
2015-02-06 01:17:40 +0100

    Display acceptance / rejection.

--- game/views.py
+++ game/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render,RequestContext,render_to_response
+from django.shortcuts import RequestContext, render_to_response
+from models import acceptWord,normalizeWord
 
 
 def submissionForm(request):
+    word = request.POST.get('word',None)
+    score = -1
+    if word is not None:
+      if acceptWord(normalizeWord(word)):
+          score = 1
+
     return render_to_response('submissionForm.html',
-            {'accepted':True},
+            {'score':score,
+             'word':word},
             context_instance=RequestContext(request))
